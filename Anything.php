6c4aa02b709d7d346e3674705fbecded3153298d
kvora97 <kenil_vora@hotmail.com>
2017-10-31 00:51:04 +0000

    Sensors reading and printing values

--- Anything.php
+++ Anything.php
@@ -1,9 +1,28 @@
 <?php
 
-  $conn = new MongoClient (["mongodb://<dbuser>:<dbpassword>@ds044689.mlab.com:44689/ambilamp"]); // paste your db URL
-  $db = $conn->[ambilamp];
-  $collection = $db->[AmbiLamp]; // paste collection name
-  $newData = array( 'num' => 130, 'val' => 57, 'time' => '8pm' );
+  $conn = new MongoClient ("mongodb://AmbiLamp:admin@ds044689.mlab.com:44689/ambilamp"); // paste your db URL
+  $db = $conn->ambilamp;
+  $collection = $db->AmbiLamp; // paste collection name
+  $newData = array( 'num' => 131, 'val' => 57, 'time' => '8pm' );
   $collection->insert($newData);
+
+  $cursor = $collection->find();
+
+  echo "First loop: <br>";
+  foreach ($cursor as $doc) {
+
+    echo $doc['num'] . "<br>";
+
+  }
+  
+  echo "<br>";
+
+  $cursor = $collection->find(array('num' => 131));
+  echo "Second loop: <br>";
+  foreach ($cursor as $doc) {
+
+    echo $doc['num'] . "<br>";
+
+  }
 
 ?>
